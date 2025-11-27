346e8fad346dc4850cc0e5a1895c02a7babf819e
Erifluffball <101990800+Erifluffball@users.noreply.github.com>
2025-11-27 20:53:47 +0000

    Updated Scripts with Solution
    GoToHospital.cs wasn't working

--- Assets/Patient.cs
+++ Assets/Patient.cs
@@ -1,20 +1,15 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Patient : GAgent
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    protected override void Start()
+    // Start is called before the first frame update
+    new void Start()
     {
         base.Start();
         SubGoal s1 = new SubGoal("isWaiting", 1, true);
-        goals.Add(s1, 3);
+        goals.Add(s1, 3); 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
