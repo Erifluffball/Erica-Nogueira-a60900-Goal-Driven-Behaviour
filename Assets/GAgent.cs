a350e3a3d79df92b3d1339cb5f24cb934001659f
Erifluffball <101990800+Erifluffball@users.noreply.github.com>
2025-11-27 19:27:20 +0000

    Patient.cs added

--- Assets/GAgent.cs
+++ Assets/GAgent.cs
@@ -30,7 +30,7 @@ public class GAgent : MonoBehaviour
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    protected virtual void Start()
     {
         GAction[] acts = this.GetComponents<GAction>();
         foreach (GAction a in acts)
